be6ace7d6f1fb673892313b691d8dae1032ff20e
g000001 <chiba.masaomi@gmail.com>
2012-01-11 12:27:54 +0900

    fix: package definition

--- package.lisp
+++ package.lisp
@@ -3,10 +3,10 @@
 (cl:in-package :cl-user)
 
 (defpackage :srfi-61
+  (:use)
   (:export :cond))
 
 (defpackage :srfi-61-internal
   (:use :srfi-61 :cl :fiveam :mbe)
   (:shadowing-import-from :srfi-61 :cond)
   (:shadow :t))
-
